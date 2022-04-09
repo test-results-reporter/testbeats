e6abe1ade7263e02bab22763e412a74efc819833
Anudeep <asa.anudeep4@gmail.com>
2022-04-09 18:25:50 +0530

    added types

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-const publish = require('./commands/publish');
+const cmd_publish = require('./commands/publish');
+
+function publish(options) {
+  return cmd_publish.run(options);
+}
 
 module.exports = {
   publish
