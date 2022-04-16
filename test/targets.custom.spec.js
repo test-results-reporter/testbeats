9e048a30d0b2a9ea89f310a8fbaec4ba70094e2d
Anudeep <asa.anudeep4@gmail.com>
2022-04-16 14:31:27 +0530

    docs: update readme

--- test/targets.custom.spec.js
+++ test/targets.custom.spec.js
@@ -7,28 +7,7 @@ describe('targets - custom', () => {
   it('custom target', async () => {
     const id1 = mock.addInteraction('get custom');
     await publish({
-      config: {
-        "reports": [
-          {
-            "targets": [
-              {
-                "name": "custom",
-                "inputs": {
-                  "path": "test/data/custom-test.js"
-                }
-              }
-            ],
-            "results": [
-              {
-                "type": "junit",
-                "files": [
-                  "test/data/junit/single-suite.xml"
-                ]
-              }
-            ]
-          }
-        ]
-      }
+      config: 'test/data/configs/custom-target.json'
     });
     assert.equal(mock.getInteraction(id1).exercised, true);
   });
