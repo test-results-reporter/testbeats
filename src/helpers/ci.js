2687fff25c27096131a6588eb6d6e94b2d8c7d33
Anudeep <asa.anudeep4@gmail.com>
2024-03-03 14:42:13 +0530

    feat: add ci information

--- src/helpers/ci.js
+++ src/helpers/ci.js
@@ -1,22 +1,61 @@
+const ENV = process.env;
+
 function getCIInformation() {
-  if (process.env.GITHUB_ACTIONS) {
+  if (ENV.GITHUB_ACTIONS) {
     return getGitHubActionsInformation();
+  }
+  if (ENV.JENKINS_URL) {
+    return getJenkinsInformation();
+  }
+  if (ENV.SYSTEM_TEAMFOUNDATIONCOLLECTIONURI) {
+    return getAzureDevOpsInformation();
   }
 }
 
 function getGitHubActionsInformation() {
   return {
     ci: 'GITHUB_ACTIONS',
-    repository_url: process.env.GITHUB_SERVER_URL + '/' + process.env.GITHUB_REPOSITORY,
-    repository_name: process.env.GITHUB_REPOSITORY,
-    repository_ref: process.env.GITHUB_REF,
-    build_url: process.env.GITHUB_SERVER_URL + '/' + process.env.GITHUB_REPOSITORY + '/commit/' + process.env.GITHUB_SHA + '/checks/' + process.env.GITHUB_RUN_ID,
-    build_number: process.env.GITHUB_RUN_NUMBER,
-    build_name: process.env.GITHUB_WORKFLOW,
+    git: 'GITHUB',
+    repository_url: ENV.GITHUB_SERVER_URL + '/' + ENV.GITHUB_REPOSITORY,
+    repository_name: ENV.GITHUB_REPOSITORY,
+    repository_ref: ENV.GITHUB_REF,
+    repository_commit_sha: ENV.GITHUB_SHA,
+    build_url: ENV.GITHUB_SERVER_URL + '/' + ENV.GITHUB_REPOSITORY + '/commit/' + ENV.GITHUB_SHA + '/checks/' + ENV.GITHUB_RUN_ID,
+    build_number: ENV.GITHUB_RUN_NUMBER,
+    build_name: ENV.GITHUB_WORKFLOW,
+    user: ENV.GITHUB_ACTOR,
   }
 }
 
-console.log(getCIInformation())
+function getAzureDevOpsInformation() {
+  return {
+    ci: 'AZURE_DEVOPS_PIPELINES',
+    git: 'AZURE_DEVOPS_REPOS',
+    repository_url: ENV.BUILD_REPOSITORY_URI,
+    repository_name: ENV.BUILD_REPOSITORY_NAME,
+    repository_ref: ENV.BUILD_SOURCEBRANCH,
+    repository_commit_sha: ENV.BUILD_SOURCEVERSION,
+    build_url: ENV.SYSTEM_TEAMFOUNDATIONCOLLECTIONURI + ENV.SYSTEM_TEAMPROJECT + '/_build/results?buildId=' + ENV.BUILD_BUILDID,
+    build_number: ENV.BUILD_BUILDNUMBER,
+    build_name: ENV.BUILD_DEFINITIONNAME,
+    user: ENV.BUILD_REQUESTEDFOR
+  }
+}
+
+function getJenkinsInformation() {
+  return {
+    ci: 'JENKINS',
+    git: '',
+    repository_url: ENV.GIT_URL || ENV.GITHUB_URL || ENV.BITBUCKET_URL,
+    repository_name: ENV.JOB_NAME,
+    repository_ref: ENV.BRANCH || ENV.BRANCH_NAME,
+    repository_commit_sha: ENV.GIT_COMMIT || ENV.GIT_COMMIT_SHA || ENV.GITHUB_SHA || ENV.BITBUCKET_COMMIT,
+    build_url: ENV.BUILD_URL,
+    build_number: ENV.BUILD_NUMBER,
+    build_name: ENV.JOB_NAME,
+    user: ENV.USER || ENV.USERNAME
+  }
+}
 
 module.exports = {
   getCIInformation
