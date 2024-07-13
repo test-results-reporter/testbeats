53ef6549bd61781cfc2399b41e5d2e2e7a4018fe
Anudeep <asa.anudeep4@gmail.com>
2024-07-13 10:51:45 +0530

    chore: refactor custom extension (#221)
    * chore: refactor custom extension
    
    * fix: custom target assertions

--- test/data/custom/custom-target-runner.js
+++ test/data/custom/custom-target-runner.js
@@ -1,9 +1,8 @@
 const p6 = require('pactum');
 const assert = require('assert');
 
-async function run({ target, extension, result }) {
-  assert.equal(target.name, 'teams');
-  assert.equal(extension.name, 'custom');
+async function run({ target, result }) {
+  assert.equal(target.name, 'custom');
   assert.equal(result.name, 'Default suite');
   await p6.spec().get('http://localhost:9393/custom');
 }
